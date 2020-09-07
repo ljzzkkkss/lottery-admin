4f9843a2185b859a7b95270f81d55676cf40b56c
cymengWiscom <1234qwer>
2020-09-07 16:55:37 +0800

    Initial commit

---
+++ src/main/java/com/ljzzkkkss/lottery/admin/LotteryAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ljzzkkkss.lottery.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljzzkkkss/lottery/admin/LotteryAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljzzkkkss.lottery.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
