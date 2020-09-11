f7662d32f1ff22fffab0a224a43e97cbe42a7b3e
cymengWiscom <1234qwer>
2020-09-11 17:04:51 +0800

    文章编辑功能临时提交

---
+++ src/main/java/com/ljzzkkkss/lottery/admin/controller/FileController.java
@@ -0,0 +1,40 @@
+package com.ljzzkkkss.lottery.admin.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class FileController {
+    @Value("${file.path}")
+    private String path;
+
+    @PostMapping("/file/upload")
+    public ReturnBody upload(@RequestParam("file") MultipartFile file) throws IOException {
+        if(-1 == file.getOriginalFilename().lastIndexOf('.')){
+            return ReturnType.INVALID_FILE;
+        }
+        SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String date = dayFormat.format(new Date());
+        String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.'));
+        String filePath = path + date + "/";
+        String fileName = System.currentTimeMillis() + suffix;
+        File storeFile = new File( filePath + fileName);
+        if(!storeFile.getParentFile().exists()){
+            storeFile.getParentFile().mkdirs();
+        }
+        storeFile.createNewFile();
+        try (FileOutputStream out = new FileOutputStream(storeFile)) {
+            out.write(file.getBytes());
+        }
+        return new ReturnBody(filePath + fileName);
+    }
+}
