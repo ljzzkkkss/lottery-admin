c9fd20ee4ac933b5e89150b22f2da61be3e5f557
ljzzkkkss <793958447@qq.com>
2020-09-13 21:20:00 +0800

    调整表结构

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,17 +1,9 @@
 CREATE TABLE `admin_user` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `username` VARCHAR(45) NULL COMMENT '用户名',
-  `password` VARCHAR(45) NULL COMMENT '密码',
-  PRIMARY KEY (`id`))
-COMMENT = '管理员用户表';
-
-CREATE TABLE `user` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `username` VARCHAR(45) NULL COMMENT '用户名',
-  `password` VARCHAR(45) NULL COMMENT '密码',
-  `phone` VARCHAR(45) NULL COMMENT '手机号',
-  PRIMARY KEY (`id`))
-COMMENT = '用户表';
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `username` varchar(45) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(45) DEFAULT NULL COMMENT '密码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='管理员用户表';
 
 CREATE TABLE `article` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '文章id',
@@ -25,32 +17,63 @@ CREATE TABLE `article` (
   `status` tinyint(4) DEFAULT NULL COMMENT '文章状态 0 草稿，1 发表，2 下架',
   `image` varchar(255) DEFAULT NULL COMMENT '列表缩略图',
   PRIMARY KEY (`id`)
-) COMMENT='文章表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='文章表';
 
+CREATE TABLE `banner` (
+  `id` int(11) NOT NULL COMMENT 'banner id',
+  `img` varchar(255) DEFAULT NULL COMMENT 'banner Url',
+  `url` varchar(255) DEFAULT NULL COMMENT '跳转url',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='首页banner表';
+
+CREATE TABLE `help` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '帮助中心id',
+  `title` varchar(255) DEFAULT NULL COMMENT '帮助标题',
+  `content` text COMMENT '帮助中心内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='帮助中心表';
+
+CREATE TABLE `log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '日志id',
+  `content` varchar(45) DEFAULT NULL COMMENT '日志内容',
+  `phone` varchar(45) DEFAULT NULL COMMENT '手机号',
+  `logTime` datetime DEFAULT NULL COMMENT '日志时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='日志表';
 
 CREATE TABLE `match` (
-  `id` BIGINT NOT NULL COMMENT '赛事id',
-  `mainTeam` VARCHAR(45) NULL COMMENT '主队',
-  `clientTeam` VARCHAR(45) NULL COMMENT '客队',
-  `halfScore` VARCHAR(45) NULL COMMENT '半场比分',
-  `totalScore` VARCHAR(45) NULL COMMENT '全场比分',
-  `match` VARCHAR(45) NULL COMMENT '所属联赛',
-  `round` VARCHAR(45) NULL COMMENT '轮次',
-  `matchTime` DATETIME NULL COMMENT '比赛时间',
-  `status` VARCHAR(45) NULL COMMENT '比赛状态',
+  `id` bigint(20) NOT NULL COMMENT '赛事id',
+  `mainTeam` varchar(45) DEFAULT NULL COMMENT '主队',
+  `clientTeam` varchar(45) DEFAULT NULL COMMENT '客队',
+  `halfScore` varchar(45) DEFAULT NULL COMMENT '半场比分',
+  `totalScore` varchar(45) DEFAULT NULL COMMENT '全场比分',
+  `match` varchar(45) DEFAULT NULL COMMENT '所属联赛',
+  `round` varchar(45) DEFAULT NULL COMMENT '轮次',
+  `matchTime` datetime DEFAULT NULL COMMENT '比赛时间',
+  `status` varchar(45) DEFAULT NULL COMMENT '比赛状态',
   PRIMARY KEY (`id`),
-  INDEX `INDEX_TEAM` (`mainTeam` ASC, `clientTeam` ASC, `matchTime` ASC) INVISIBLE,
-  INDEX `INDEX_TIME` (`matchTime` ASC) VISIBLE)
-COMMENT = '赛事表';
+  KEY `INDEX_TEAM` (`mainTeam`,`clientTeam`,`matchTime`) /*!80000 INVISIBLE */,
+  KEY `INDEX_TIME` (`matchTime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='赛事表';
+
+CREATE TABLE `note` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '留言id',
+  `userId` int(11) DEFAULT NULL COMMENT '用户id',
+  `content` text COMMENT '留言内容',
+  `optionalId` int(11) DEFAULT NULL COMMENT '方案id',
+  `noteTime` datetime DEFAULT NULL COMMENT '留言日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='留言表';
 
 CREATE TABLE `odd` (
-  `id` BIGINT NOT NULL COMMENT '赔率id',
-  `matchId` BIGINT NOT NULL COMMENT '赛事id',
-  `category` VARCHAR(45) NULL COMMENT '分类（非让球，主-1，比分 等）',
-  `rate` VARCHAR(45) NULL COMMENT '赔率',
-  `content` VARCHAR(45) NULL COMMENT '赔率内容（胜，负，0:1等）',
-  PRIMARY KEY (`id`))
-COMMENT = '赔率表';
+  `id` bigint(20) NOT NULL COMMENT '赔率id',
+  `matchId` bigint(20) NOT NULL COMMENT '赛事id',
+  `category` varchar(45) DEFAULT NULL COMMENT '分类（非让球，主-1，比分 等）',
+  `rate` varchar(45) DEFAULT NULL COMMENT '赔率',
+  `content` varchar(45) DEFAULT NULL COMMENT '赔率内容（胜，负，0:1等）',
+  `single` tinyint(4) DEFAULT NULL COMMENT '是否单关 0否 1是',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='赔率表';
 
 CREATE TABLE `optional` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '方案id',
@@ -60,17 +83,44 @@ CREATE TABLE `optional` (
   `optionDate` datetime DEFAULT NULL COMMENT '投注日期',
   `name` varchar(45) DEFAULT NULL COMMENT '投注名称',
   `status` tinyint(4) DEFAULT NULL COMMENT '方案状态 0 未提交，1 提交，2 已投注',
+  `times` int(11) DEFAULT NULL COMMENT '倍数',
+  `game` varchar(255) DEFAULT NULL COMMENT '玩法',
+  `noteNumber` int(11) DEFAULT NULL COMMENT '注数',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='自选方案表';
 
 CREATE TABLE `optional_detail` (
-  `id` int(11) NOT NULL,
-  `matchId` bigint(20) NOT NULL,
+  `id` int(11) NOT NULL COMMENT '方案详情id',
+  `matchId` bigint(20) NOT NULL COMMENT '赛事id',
   `optionalId` int(11) DEFAULT NULL COMMENT '方案id',
   `category` varchar(45) DEFAULT NULL COMMENT '分类（非让球，主-1，比分 等）',
   `rate` varchar(45) DEFAULT NULL COMMENT '赔率',
   `content` varchar(45) DEFAULT NULL COMMENT '赔率内容（胜，负，0:1等）',
-  `times` int(11) DEFAULT NULL COMMENT '倍数',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='自选方案详情表';
 
+CREATE TABLE `recommend` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '推荐方案id',
+  `articleId` int(11) DEFAULT NULL COMMENT '文章id',
+  `matchId` bigint(20) DEFAULT NULL COMMENT '赛事id',
+  `category` varchar(45) DEFAULT NULL COMMENT '分类（非让球，主-1，比分 等）',
+  `rate` varchar(45) DEFAULT NULL COMMENT '赔率',
+  `content` varchar(45) DEFAULT NULL COMMENT '赔率内容（胜，负，0:1等）',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='推荐方案表';
+
+CREATE TABLE `reply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '回复id',
+  `content` text COMMENT '自动回复内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='自动回复表';
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `username` varchar(45) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(45) DEFAULT NULL COMMENT '密码',
+  `phone` varchar(45) DEFAULT NULL COMMENT '手机号',
+  `registerTime` datetime DEFAULT NULL COMMENT '注册日期',
+  `lastLogin` datetime DEFAULT NULL COMMENT '最近登录时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表';
